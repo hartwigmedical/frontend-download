d901f1f0883d17d8ff10be16bbb4194c94c2e372
Thijs Houtenbos <thoutenbos@schubergphilis.com>
2018-05-22 16:13:57 +0200

    Optimize stub data

--- stubs/runs-details.js
+++ stubs/runs-details.js
@@ -1,20 +1,20 @@
 var runsDetailsStub = [
   {
     "id": 123,
-    "name": "171026_HMFreg0175_FR11471080-A01_CF-DH10",
+    "name": "171026_HMFtest_AA11110000-A001",
     "files": [
       {
-        "filename": "LT-banchu-red-fire_H77WTCCXY_S28_L001_R1_001.fastq.gz",
+        "filename": "HMFtest_AA11110000_S28_L001_R1_001.fastq.gz",
         "filesize": 2035434955,
         "name": "FastQ",
-        "fileid": 1675732,
+        "fileid": 91,
         "tier_status": "RESTORE"
       },
       {
-        "filename": "LT-banchu-red-fire_H77WTCCXY_S28_L001_R2_001.fastq.gz",
+        "filename": "HMFtest_AA11110000_S28_L001_R2_001.fastq.gz",
         "filesize": 2035434955,
         "name": "FastQ",
-        "fileid": 1675733,
+        "fileid": 92,
         "tier_status": "COLD",
         "restore_time": 50
       }
@@ -27,7 +27,7 @@ var runsDetailsStub = [
         "filename": "PatientReport.pdf",
         "filesize": 12345,
         "name": "patient_report",
-        "fileid": 1675737,
+        "fileid": 101,
         "tier_status": "HOT"
       }
     ]
@@ -39,28 +39,28 @@ var runsDetailsStub = [
         "filename": "161207_VAL-S00025_v1.10_SingleSample.filtered_variants.annotated.vcf",
         "filesize": 2839957935,
         "name": "germline_vcf",
-        "fileid": 861436,
+        "fileid": 111,
         "tier_status": "HOT"
       },
       {
         "filename": "161207_VAL-S00025_v1.10_SingleSample.filtered_variants.annotated.vcf.idx",
         "filesize": 1548284,
         "name": "FastQ",
-        "fileid": 861456,
+        "fileid": 112,
         "tier_status": "RESTORE"
       },
       {
         "filename": "161207_VAL-S00025_v1.10_SingleSample.bamMetrics.pdf",
         "filesize": 84770,
         "name": "FastQ",
-        "fileid": 861468,
+        "fileid": 113,
         "tier_status": "RESTORE"
       },
       {
         "filename": "GIAB12878b_dedup.realigned.bam.bai",
         "filesize": 8834784,
         "name": "FastQ",
-        "fileid": 861942,
+        "fileid": 114,
         "tier_status": "COLD",
         "restore_time": 24
       },
@@ -68,21 +68,21 @@ var runsDetailsStub = [
         "filename": "GIAB12878b_dedup.realigned.bam",
         "filesize": 69238369717,
         "name": "ref_sample_bam",
-        "fileid": 861952,
+        "fileid": 115,
         "tier_status": "HOT"
       },
       {
         "filename": "GIAB12878b_dedup.realigned.g.vcf.gz",
         "filesize": 7096760025,
         "name": "ref_sample_gvcf",
-        "fileid": 861658,
+        "fileid": 116,
         "tier_status": "HOT"
       },
       {
         "filename": "GIAB12878b_dedup.realigned.g.vcf.gz.tbi",
         "filesize": 3947347,
         "name": "ref_sample_gvcf_index",
-        "fileid": 861660,
+        "fileid": 117,
         "tier_status": "HOT"
       }
     ]
@@ -94,63 +94,63 @@ var runsDetailsStub = [
         "filename": "161224_VAL-S00032_v1.11_KG_extras.tar.gz",
         "filesize": 56516838,
         "name": "extras_tar",
-        "fileid": 974786,
+        "fileid": 121,
         "tier_status": "HOT"
       },
       {
         "filename": "161224_VAL-S00032_v1.11_KG_extras.zip",
         "filesize": 56518134,
         "name": "extras_zip",
-        "fileid": 974812,
+        "fileid": 122,
         "tier_status": "HOT"
       },
       {
         "filename": "161224_VAL-S00032_v1.11_KG.filtered_variants.annotated.vcf",
         "filesize": 2853754546,
         "name": "germline_vcf",
-        "fileid": 974808,
+        "fileid": 123,
         "tier_status": "HOT"
       },
       {
         "filename": "161224_VAL-S00032_v1.11_KG.filtered_variants.annotated.vcf.idx",
         "filesize": 1548264,
         "name": "germline_vcf_index",
-        "fileid": 974800,
+        "fileid": 124,
         "tier_status": "HOT"
       },
       {
         "filename": "161224_VAL-S00032_v1.11_KG.bamMetrics.pdf",
         "filesize": 84695,
         "name": "qc",
-        "fileid": 975058,
+        "fileid": 125,
         "tier_status": "HOT"
       },
       {
         "filename": "GIAB12878a_dedup.bam.bai",
         "filesize": 8820384,
         "name": "ref_sample_bai",
-        "fileid": 975026,
+        "fileid": 126,
         "tier_status": "HOT"
       },
       {
         "filename": "GIAB12878a_dedup.bam",
         "filesize": 64566771919,
         "name": "ref_sample_bam",
-        "fileid": 975032,
+        "fileid": 127,
         "tier_status": "HOT"
       },
       {
         "filename": "GIAB12878a_dedup.recalibrated.g.vcf.gz",
         "filesize": 5145988432,
         "name": "ref_sample_gvcf",
-        "fileid": 975244,
+        "fileid": 128,
         "tier_status": "HOT"
       },
       {
         "filename": "GIAB12878a_dedup.recalibrated.g.vcf.gz.tbi",
         "filesize": 3952007,
         "name": "ref_sample_gvcf_index",
-        "fileid": 975246,
+        "fileid": 129,
         "tier_status": "HOT"
       }
     ]
@@ -162,91 +162,91 @@ var runsDetailsStub = [
         "filename": "GIAB_REGRESSION-170221_extras.zip",
         "filesize": 381354,
         "name": "extras_zip",
-        "fileid": 1669267
+        "fileid": 131
       },
       {
         "filename": "GIAB_REGRESSION-170221.annotated.vcf",
         "filesize": 3970182567,
         "name": "germline_vcf",
-        "fileid": 1669255
+        "fileid": 132
       },
       {
         "filename": "GIAB_REGRESSION-170221.annotated.vcf.idx",
         "filesize": 1548256,
         "name": "germline_vcf_index",
-        "fileid": 1669261
+        "fileid": 133
       },
       {
         "filename": "GIAB_REGRESSION-170221.bamMetrics.pdf",
         "filesize": 116201,
         "name": "qc",
-        "fileid": 1674322
+        "fileid": 134
       },
       {
         "filename": "CPCT11111111R_dedup.realigned.bam.bai",
         "filesize": 8862520,
         "name": "ref_sample_bai",
-        "fileid": 1673386
+        "fileid": 135
       },
       {
         "filename": "CPCT11111111R_dedup.realigned.bam",
         "filesize": 85953178962,
         "name": "ref_sample_bam",
-        "fileid": 1673356
+        "fileid": 136
       },
       {
         "filename": "CPCT11111111R.g.vcf.gz",
         "filesize": 14565734939,
         "name": "ref_sample_gvcf",
-        "fileid": 1673233
+        "fileid": 137
       },
       {
         "filename": "CPCT11111111R.g.vcf.gz.tbi",
         "filesize": 3794908,
         "name": "ref_sample_gvcf_index",
-        "fileid": 1673236
+        "fileid": 138
       },
       {
         "filename": "CPCT11111111R_CPCT11111111T_melted.vcf",
         "filesize": 1213624890,
         "name": "somatic_vcf",
-        "fileid": 1674499
+        "fileid": 139
       },
       {
         "filename": "CPCT11111111R_CPCT11111111T_melted.vcf.idx",
         "filesize": 1548190,
         "name": "somatic_vcf_index",
-        "fileid": 1674517
+        "fileid": 140
       },
       {
         "filename": "CPCT11111111T_dedup.realigned.bam.bai",
         "filesize": 9661088,
         "name": "tumor_sample_bai",
-        "fileid": 1673848
+        "fileid": 141
       },
       {
         "filename": "CPCT11111111T_dedup.realigned.bam",
         "filesize": 250886024970,
         "name": "tumor_sample_bam",
-        "fileid": 1673875
+        "fileid": 142
       },
       {
         "filename": "CPCT11111111T.g.vcf.gz",
         "filesize": 2197678598,
         "name": "tumor_sample_gvcf",
-        "fileid": 1673242
+        "fileid": 143
       },
       {
         "filename": "CPCT11111111T.g.vcf.gz.tbi",
         "filesize": 3571713,
         "name": "tumor_sample_gvcf_index",
-        "fileid": 1673239
+        "fileid": 144
       },
       {
         "filename": "GIAB_REGRESSION-170221_extras.tar.gz",
         "filesize": 365438,
         "name": "extras_tar",
-        "fileid": 1675730
+        "fileid": 145
       }
     ]
   }
